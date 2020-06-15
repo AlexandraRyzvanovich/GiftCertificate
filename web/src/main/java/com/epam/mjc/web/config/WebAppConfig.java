a57b4405ed194588e3d1b61272ce4ca78f611f55
Alexandra Ryzvanovich <alexandraryzvanovich@gmail.com>
2020-06-15 03:32:11 +0300

    Project structure

--- web/src/main/java/com/epam/mjc/web/config/WebAppConfig.java
+++ web/src/main/java/com/epam/mjc/web/config/WebAppConfig.java
@@ -1,16 +1,10 @@
 package com.epam.mjc.web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.BeanNameViewResolver;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 
 @Configuration
