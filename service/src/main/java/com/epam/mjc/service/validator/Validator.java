09d30a7465a076c4b72750769979de30a854c1e1
Alexandra Ryzvanovich <alexandraryzvanovich@gmail.com>
2020-07-15 03:56:36 +0300

    Certificate update fix

--- service/src/main/java/com/epam/mjc/service/validator/Validator.java
+++ service/src/main/java/com/epam/mjc/service/validator/Validator.java
@@ -1,45 +1,18 @@
 package com.epam.mjc.service.validator;
 
-import com.epam.mjc.dao.entity.GiftCertificateEntity;
 import com.epam.mjc.dao.entity.OrderEntity;
 import com.epam.mjc.dao.entity.Tag;
 import com.epam.mjc.dao.entity.User;
 import com.epam.mjc.service.exception.ValidationException;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-
 @Component
 public class Validator {
-    private static final BigDecimal MAX_PRICE_VALUE = new BigDecimal(10000) ;
-    private static final BigDecimal MIN_PRICE_VALUE = new BigDecimal(0);
-    private static final int DESCRIPTION_MAX_LENGTH = 200;
     private static final int CERTIFICATE_NAME_MAX_LENGTH = 50;
     private static final int TAG_NAME_MAX_LENGTH = 50;
-    private static final int VALID_DAYS_MIN_VALUE = 0;
-    private static final int VALID_DAYS_MAX_VALUE = 500;
     private static final int USER_NAME_MAX_LENGTH = 50;
     private static final int USER_SURNAME_MAX_LENGTH = 50;
 
-    public static void validateCertificate(GiftCertificateEntity giftCertificateEntity) {
-        String name =  giftCertificateEntity.getName();
-        String description = giftCertificateEntity.getDescription();
-        BigDecimal price = giftCertificateEntity.getPrice();
-        Integer validDays = giftCertificateEntity.getValidDays();
-
-        if(name == null || name.length() > CERTIFICATE_NAME_MAX_LENGTH || name.isEmpty()) {
-            throw new ValidationException("Gift certificate name" + name + "is incorrect. Requirements: not empty, max length 50 characters");
-        }
-        if(description == null || description.length() > DESCRIPTION_MAX_LENGTH || description.isEmpty()) {
-            throw new ValidationException("Gift certificate description" + description + "is incorrect. Requirements: not empty, max length " + DESCRIPTION_MAX_LENGTH + " characters");
-        }
-        if(price == null || price.compareTo(MIN_PRICE_VALUE) < 0 || price.compareTo(MAX_PRICE_VALUE) > 0 ) {
-            throw new ValidationException("Gift certificate price" + price + "is incorrect. Requirements: Min value = " +  MIN_PRICE_VALUE + ", max = " + MAX_PRICE_VALUE);
-        }
-        if(validDays == null || validDays < VALID_DAYS_MIN_VALUE || validDays > VALID_DAYS_MAX_VALUE) {
-            throw new ValidationException("Gift certificate valid days field" + validDays + "is incorrect. Requirements: not empty, max value " + VALID_DAYS_MAX_VALUE);
-        }
-    }
 
     public static void validateTag(Tag tag) {
         String name =  tag.getName();
